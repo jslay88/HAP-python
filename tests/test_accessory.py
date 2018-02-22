bbf648dcabe6dc6eb28b7da5dc9ab8c7502c1218
jslay <justin.slay@gmail.com>
2018-02-21 23:32:06 -0700

    Added RegEx tests to Accesorry and Bridge adding Bridge test

--- tests/test_accessory.py
+++ tests/test_accessory.py
@@ -2,9 +2,11 @@
 Tests for pyhap.accessory
 """
 import pytest
+import re
 
 import pyhap.accessory as accessory
 import pyhap.loader as loader
+
 
 class TestAccessory(object):
 
@@ -18,10 +20,27 @@ class TestAccessory(object):
         acc.add_service(service)
         char.set_value(25, should_notify=True)
 
+    def test_setup_id(self):
+        pattern = re.compile('[A-Z0-9]{4}')
+        acc = accessory.Accessory('Test Accessory')
+        assert pattern.match(acc.setup_id) is not None
+
+    def test_pincode(self):
+        pattern = re.compile(b'\d{3}-\d{2}-\d{3}')
+        acc = accessory.Accessory('Test Accessory')
+        assert pattern.match(acc.pincode) is not None
+
+    def test_xhm_uri(self):
+        acc = accessory.Accessory('Test Accessory')
+        pattern = re.compile('X-HM://[A-Z0-9]{9}' + acc.setup_id)
+        assert pattern.match(acc.xhm_uri) is not None
+
+
 class TestBridge(TestAccessory):
 
     def test_init(self):
         bridge = accessory.Bridge("Test Bridge")
+        assert bridge.category == accessory.Category.BRIDGE
 
     def test_add_accessory(self):
         bridge = accessory.Bridge("Test Bridge")
@@ -45,3 +64,9 @@ class TestBridge(TestAccessory):
         bridge.add_accessory(acc_1)
         with pytest.raises(ValueError):
             bridge.add_accessory(acc_2)
+
+    def test_add_bridge_to_bridge(self):
+        bridge = accessory.Bridge('Test Bridge')
+        bridge_acc = accessory.Bridge('Test Bridge Acc')
+        with pytest.raises(ValueError):
+            bridge.add_accessory(bridge_acc)
