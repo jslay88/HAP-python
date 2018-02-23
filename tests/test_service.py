97784a9b8d88737459f9084d726b597f96fa5430
jslay <justin.slay@gmail.com>
2018-02-22 23:22:10 -0700

    More Tests

--- tests/test_service.py
+++ tests/test_service.py
@@ -7,16 +7,19 @@ import pytest
 
 import pyhap.service as service
 from pyhap.characteristic import Characteristic, HAP_FORMAT, HAP_PERMISSIONS
+from pyhap.accessory import IIDManager
 
 CHAR_PROPS = {
     "Format": HAP_FORMAT.INT,
     "Permissions": HAP_PERMISSIONS.READ,
 }
 
+
 def get_chars():
     c1 = Characteristic("Char 1", uuid.uuid1(), CHAR_PROPS)
     c2 = Characteristic("Char 2", uuid.uuid1(), CHAR_PROPS)
     return [c1, c2]
+
 
 def test_add_characteristic():
     serv = service.Service(uuid.uuid1(), "Test Service")
@@ -25,6 +28,7 @@ def test_add_characteristic():
     for c in chars:
         assert serv.get_characteristic(c.display_name, check_optional=False) == c
 
+
 def test_add_opt_characteristic():
     serv = service.Service(uuid.uuid1(), "Test Service")
     chars = get_chars()
@@ -32,5 +36,12 @@ def test_add_opt_characteristic():
     for c in chars:
         assert serv.get_characteristic(c.display_name, check_optional=True) == c
 
+
 def test_to_HAP():
-    pass # TODO:
+    serv = service.Service('83B45914-1861-11E8-85E7-1C1B0D902B9B', 'Test Service')
+    iid = IIDManager()
+    hap_rep = serv.to_HAP(iid)
+    print(hap_rep, flush=True)
+    assert (hap_rep['iid'] is None
+            and hap_rep['type'] == '83B45914-1861-11E8-85E7-1C1B0D902B9B'
+            and hap_rep['characteristics'] == [])
